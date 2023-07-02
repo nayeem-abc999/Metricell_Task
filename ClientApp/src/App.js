01332a1c4a8ca814cf201466edb27c5fab645c87
nayeem <nayeem.abc999@gmail.com>
2023-07-02 20:44:52 +0100

    Completed the task

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,28 +1,89 @@
 import React, { useState, useEffect } from 'react';
 
-export default function () {
+export default function App() {
+  const [employees, setEmployees] = useState([]);
 
-    async function getEmployees() {
-        return fetch("/employees").then(response => response.json());
-    }
+  useEffect(() => {
+    getEmployees().then(data => setEmployees(data));
+  }, []);
 
-    async function createEmployee(name, value) {
-        return fetch("/employees", {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name: name, value: value })
-        });
-    }
+  async function getEmployees() {
+    return fetch('/employees').then(response => response.json());
+  }
 
-    async function updateEmployee(name, value) {
-        return fetch("/employees", {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name: name, value: value })
-        });
-    }
+  async function createEmployee(name, value) {
+    await fetch('/employees', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: name, value: value })
+    });
+    // After creating the employee, fetch the updated employee list
+    getEmployees().then(data => setEmployees(data));
+  }
 
-    return (
-        <div>Complete your app here</div>
-    );
+  async function updateEmployee(name, value) {
+    await fetch('/employees', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: name, value: value })
+    });
+    // After updating the employee, fetch the updated employee list
+    getEmployees().then(data => setEmployees(data));
+  }
+
+  return (
+    <div>
+      <h1>Employee List</h1>
+      <ul>
+        {employees.map(employee => (
+          <li key={employee.name}>
+            {employee.name}: {employee.value}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          const { name, value } = event.target.elements;
+          createEmployee(name.value, parseInt(value.value));
+          name.value = '';
+          value.value = '';
+        }}
+      >
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Value:
+          <input type="number" name="value" />
+        </label>
+        <br />
+        <button type="submit">Create Employee</button>
+      </form>
+      <hr />
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          const { name, value } = event.target.elements;
+          updateEmployee(name.value, parseInt(value.value));
+          name.value = '';
+          value.value = '';
+        }}
+      >
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Value:
+          <input type="number" name="value" />
+        </label>
+        <br />
+        <button type="submit">Update Employee</button>
+      </form>
+    </div>
+  );
 }
